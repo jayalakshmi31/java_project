ca46ad5945bdecec00f79f94aede734ea5c88278
jayalakshmi31 <112812081+jayalakshmi31@users.noreply.github.com>
2023-12-24 10:51:34 +0530

    Add files via upload

---
+++ java_file/Alpha.java
@@ -0,0 +1,14 @@
+package jayalakshmi;
+
+public class Alpha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char firstchar='A';
+char lastchar='Z';
+for (char startchar=firstchar;startchar<=lastchar;startchar++) {
+	System.out.println(startchar+" ");
+}
+	}
+
+}
